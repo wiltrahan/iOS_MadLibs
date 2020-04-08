4c2bb3d683454e70b2f6cb97cf9075d7855c3709
Wil Trahan <twilorip@gmail.com>
2020-04-08 16:28:43 -0400

    Initial Commit

---
+++ MadLibs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MadLibs
+//
+//  Created by William Trahan on 4/8/20.
+//  Copyright © 2020 Wil Trahan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
